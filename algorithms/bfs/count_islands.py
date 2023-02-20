2fd915c137d9b3b4bb9894f3e7c84c4174078478
willb <willb@kth.se>
2023-02-20 10:20:15 +0100

    manual branch coverage count_islands, resolves #4

--- algorithms/bfs/count_islands.py
+++ algorithms/bfs/count_islands.py
@@ -37,29 +37,51 @@ Answer: 5
 """
 
 
-def count_islands(grid):
-    row = len(grid)
-    col = len(grid[0])
+def count_islands(grid, flags):
+	# 1
+	row = len(grid)
+	col = len(grid[0])
 
-    num_islands = 0
-    visited = [[0] * col for i in range(row)]
-    directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    queue = []
+	num_islands = 0
+	# 2
+	visited = [[0] * col for i in range(row)]
+	directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+	queue = []
 
-    for i in range(row):
-        for j, num in enumerate(grid[i]):
-            if num == 1 and visited[i][j] != 1:
-                visited[i][j] = 1
-                queue.append((i, j))
-                while queue:
-                    x, y = queue.pop(0)
-                    for k in range(len(directions)):
-                        nx_x = x + directions[k][0]
-                        nx_y = y + directions[k][1]
-                        if nx_x >= 0 and nx_y >= 0 and nx_x < row and nx_y < col:
-                            if visited[nx_x][nx_y] != 1 and grid[nx_x][nx_y] == 1:
-                                queue.append((nx_x, nx_y))
-                                visited[nx_x][nx_y] = 1
-                num_islands += 1
+	# 3
+	for i in range(row):
+		flags[0] = True
+		# 4
+		for j, num in enumerate(grid[i]):
+			flags[1] = True
+			# 5, 6
+			if num == 1 and visited[i][j] != 1:
+				flags[2] = True
+				visited[i][j] = 1
+				queue.append((i, j))
+				# 7
+				while queue:
+					flags[4] = True
+					x, y = queue.pop(0)
+					# 8
+					for k in range(len(directions)):
+						flags[5] = True
+						nx_x = x + directions[k][0]
+						nx_y = y + directions[k][1]
+						# 9, 10, 11, 12
+						if nx_x >= 0 and nx_y >= 0 and nx_x < row and nx_y < col:
+							flags[6] = True
+							# 13, 14
+							if visited[nx_x][nx_y] != 1 and grid[nx_x][nx_y] == 1:
+								flags[8] = True
+								queue.append((nx_x, nx_y))
+								visited[nx_x][nx_y] = 1
+							else:
+								flags[9] = True
+						else:
+							flags[7] = True
+				num_islands += 1
+			else:
+				flags[3] = True
 
-    return num_islands
+	return num_islands
--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -10,18 +10,29 @@ import unittest
 class TestCountIslands(unittest.TestCase):
 
     def test_count_islands(self):
+        flags = [False for _ in range(10)]
+
         grid_1 = [[1, 1, 1, 1, 0], [1, 1, 0, 1, 0], [1, 1, 0, 0, 0],
                   [0, 0, 0, 0, 0]]
-        self.assertEqual(1, count_islands(grid_1))
+        self.assertEqual(1, count_islands(grid_1, flags))
         grid_2 = [[1, 1, 0, 0, 0], [1, 1, 0, 0, 0], [0, 0, 1, 0, 0],
                   [0, 0, 0, 1, 1]]
-        self.assertEqual(3, count_islands(grid_2))
+        self.assertEqual(3, count_islands(grid_2, flags))
         grid_3 = [[1, 1, 1, 0, 0, 0], [1, 1, 0, 0, 0, 0], [1, 0, 0, 0, 0, 1],
                   [0, 0, 1, 1, 0, 1], [0, 0, 1, 1, 0, 0]]
-        self.assertEqual(3, count_islands(grid_3))
+        self.assertEqual(3, count_islands(grid_3, flags))
         grid_4 = [[1, 1, 0, 0, 1, 1], [0, 0, 1, 1, 0, 0], [0, 0, 0, 0, 0, 1],
                   [1, 1, 1, 1, 0, 0]]
-        self.assertEqual(5, count_islands(grid_4))
+        self.assertEqual(5, count_islands(grid_4, flags))
+
+        branches_taken = 0
+        for flag in flags:
+            if flag == True:
+                branches_taken += 1
+        branch_coverage = branches_taken/len(flags)
+
+        print("Branches taken: " + str(branches_taken))
+        print("Branch coverage: " + str(branch_coverage))
 
 
 class TestMazeSearch(unittest.TestCase):
