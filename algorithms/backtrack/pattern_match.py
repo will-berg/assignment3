741122e76b66a8487a2bd195668ca85a7eadb713
skihpper <naiikewa@gmail.com>
2023-02-20 16:09:39 +0100

    Feature - implemented flag system #8
    Implemented a flag system in order to see if all branches has been
    reached in the function

--- algorithms/backtrack/pattern_match.py
+++ algorithms/backtrack/pattern_match.py
@@ -21,22 +21,47 @@ def pattern_match(pattern, string):
     :rtype: bool
     """
     def backtrack(pattern, string, dic):
-
         if len(pattern) == 0 and len(string) > 0:
+            flags[0] = 1
             return False
+        else:
+            flags[1] = 1
 
         if len(pattern) == len(string) == 0:
+            flags[2] = 1
             return True
+        else:
+            flags[3] = 1
 
         for end in range(1, len(string)-len(pattern)+2):
             if pattern[0] not in dic and string[:end] not in dic.values():
+                flags[4] = 1
                 dic[pattern[0]] = string[:end]
                 if backtrack(pattern[1:], string[end:], dic):
+                    flags[5] = 1
                     return True
+                else:
+                    flags[6] = 1
                 del dic[pattern[0]]
             elif pattern[0] in dic and dic[pattern[0]] == string[:end]:
+                flags[7] = 1
                 if backtrack(pattern[1:], string[end:], dic):
+                    flags[8] = 1
                     return True
+                else:
+                    flags[9] = 1
+            else:
+                flags[10] = 1
+        flags[11] = 1
         return False
+    flags = [0 for i in range(12)]
+    return_value = backtrack(pattern, string, {})
+    print(flags)
+    i = 0
+    for flag in flags:
+        if flag:
+            i += 1
 
-    return backtrack(pattern, string, {})
+    ratio =  i / len(flags)
+    
+    return return_value
