85cee50220f9561702fa4c8b7ae0085729ff437b
Asken59 <michaelask@hotmail.se>
2023-02-20 14:16:55 +0100

    issue#6 Added manual branch coverage

--- algorithms/bfs/word_ladder.py
+++ algorithms/bfs/word_ladder.py
@@ -29,14 +29,26 @@ def ladder_length(begin_word, end_word, word_list):
     :type word_list: Set[str]
     :rtype: int
     """
+
+    """
+    Manual branch coverage flags
+    """
+    flags = [False for i in range(9)]
+
     if len(begin_word) != len(end_word):
+        flags[0] = True
+        print_flags(flags)
         return -1   # not possible
 
     if begin_word == end_word:
+        flags[1] = True
+        print_flags(flags)
         return 0
 
     # when only differ by 1 character
     if sum(c1 != c2 for c1, c2 in zip(begin_word, end_word)) == 1:
+        flags[2] = True
+        print_flags(flags)
         return 1
 
     begin_set = set()
@@ -45,24 +57,39 @@ def ladder_length(begin_word, end_word, word_list):
     end_set.add(end_word)
     result = 2
     while begin_set and end_set:
+        flags[3] = True
 
         if len(begin_set) > len(end_set):
+            flags[4] = True
             begin_set, end_set = end_set, begin_set
 
         next_begin_set = set()
         for word in begin_set:
+            flags[5] = True
             for ladder_word in word_range(word):
+                flags[6] = True
                 if ladder_word in end_set:
+                    flags[7] = True
+                    print_flags(flags)
                     return result
                 if ladder_word in word_list:
+                    flags[8] = True
                     next_begin_set.add(ladder_word)
                     word_list.remove(ladder_word)
         begin_set = next_begin_set
         result += 1
         # print(begin_set)
         # print(result)
+    print_flags(flags)
     return -1
 
+def print_flags(flags):
+    coverage = 0
+    for i in range(len(flags)):
+        if flags[i]:
+            coverage += 1
+    cov_ratio = coverage / len(flags)
+    print("Branch cov: ", cov_ratio)
 
 def word_range(word):
     for ind in range(len(word)):
