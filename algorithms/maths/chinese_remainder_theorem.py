d20ef51f72066fec3ad424388520c7b725e2f283
Asken59 <michaelask@hotmail.se>
2023-02-20 15:01:13 +0100

    issue#9 Added manual branch coverage metric

--- algorithms/maths/chinese_remainder_theorem.py
+++ algorithms/maths/chinese_remainder_theorem.py
@@ -5,6 +5,7 @@ from typing import List
 from algorithms.maths.gcd import gcd
 
 def solve_chinese_remainder(nums : List[int], rems : List[int]):
+
     """
     Computes the smallest x that satisfies the chinese remainder theorem
     for a system of equations.
@@ -17,26 +18,54 @@ def solve_chinese_remainder(nums : List[int], rems : List[int]):
     All numbers in nums needs to be pariwise coprime otherwise an exception is raised
     returns x: the smallest value for x that satisfies the system of equations
     """
+
+    """
+    Manual branch coverage flags
+    """
+    flags = [False for i in range(9)]
+
     if not len(nums) == len(rems):
+        flags[0] = True
+        print_flags(flags)
         raise Exception("nums and rems should have equal length")
     if not len(nums) > 0:
+        flags[1] = True
+        print_flags(flags)
         raise Exception("Lists nums and rems need to contain at least one element")
     for num in nums:
+        flags[2] = True
         if not num > 1:
+            flags[3] = True
+            print_flags(flags)
             raise Exception("All numbers in nums needs to be > 1")
     if not _check_coprime(nums):
+        flags[4] = True
+        print_flags(flags)
         raise Exception("All pairs of numbers in nums are not coprime")
     k = len(nums)
     x = 1
     while True:
+        flags[5] = True
         i = 0
         while i < k:
+            flags[6] = True
             if x % nums[i] != rems[i]:
+                flags[7] = True
                 break
             i += 1
         if i == k:
+            flags[8] = True
+            print_flags(flags)
             return x
         x += 1
+
+def print_flags(flags):
+    coverage = 0
+    for i in range(len(flags)):
+        if flags[i]:
+            coverage += 1
+    cov_ratio = coverage / len(flags)
+    print("Branch cov: ", cov_ratio)
 
 def _check_coprime(list_to_check : List[int]):
     for ind, num in enumerate(list_to_check):
